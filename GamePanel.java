e0fcacd12a0130417b8023ba9f6c798ad8a4c0df
zep <zeplulw@gmail.com>
2022-05-31 13:30:49 -0400

    initial commit

---
+++ GamePanel.java
@@ -0,0 +1,90 @@
+package snakegame;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    JLabel label = new JLabel();
+    
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        this.add(label);
+
+        startGame();
+    }
+
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawRect(0, 0, 50, 50);
+    }
+
+    public void newApple() {
+        System.out.println("ooo spooky i made an apple");
+        draw();
+    }
+
+    public void move(char direction) {
+        
+    }
+
+    public void checkApple() {
+        
+    }
+
+    public void checkCollisions() {
+        
+    }
+
+    public void gameOver(Graphics g) {
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == null) {
+        	return;
+        }
+        System.out.println(e);
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+        }
+    }
+}
