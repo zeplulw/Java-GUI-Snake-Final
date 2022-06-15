5a0e91c6905f7222ffdf638d92038ebb4c2a3f9e
Ben <74217833+zeplulw@users.noreply.github.com>
2022-06-15 08:46:47 -0400

    feat: add signature

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,5 +1,7 @@
 package snakegame;
 
+// Signature: 68747470733a2f2f6769746875622e636f6d2f7a65706c756c77
+
 public class SnakeGame {
     public static void main(String[] args) {
         new GameFrame();
